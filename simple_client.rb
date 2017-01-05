854dfa5a9fd246fe97ae933cba0103b83992273f
Harrison-Ly <harriko@gmail.com>
2017-01-05 17:22:31 +0000

    added POST method

--- simple_client.rb
+++ simple_client.rb
@@ -1,14 +1,29 @@
 require "socket"
+require "json"
 
 hostname = "localhost"
 port = 8080
 path = "/index.html"
 
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+puts "What request do you want to send?"
+ans = gets.chomp
+
+request = ""
+if ans == "GET"
+  request = "GET #{path} HTTP/1.0\r\n\r\n"
+elsif ans == "POST"
+  hash = {}
+  puts "what is you name?"
+  name = gets.chomp
+  puts "what is your email?"
+  email = gets.chomp
+  hash[:viking] = {:name => name, :email => email}
+  request = "#{ans} /post HTTP/1.0\r\nContent-Length: #{hash.to_json.length}\r\n#{hash.to_json}"
+end
 
 s = TCPSocket.open(hostname, port)
 s.puts(request)
 while line = s.gets
-  puts line.chop
+  puts line
 end
 s.close
--- simple_server.rb
+++ simple_server.rb
@@ -1,10 +1,13 @@
 require "socket"
+require "json"
 
 server = TCPServer.new("localhost", 8080)
+puts "server running..."
+
 loop {
   connection = server.accept
-
-  method, path, ver = connection.gets.split(" ")
+  method, path, ver, content = connection.gets.split(" ")
+  header = connection.gets
   if method == "GET"
     if path == "/index.html"
       connection.puts "HTTP/1.0 200 OK"
@@ -18,6 +21,17 @@ loop {
     else
       connection.puts "HTTP/1.0 404 Not Found"
     end
+  elsif method == "POST"
+      if path == "/post"
+        params = JSON.parse(connection.gets)
+        output = ""
+        params["viking"].each { |k, v| output << "<li>#{k}: #{v}</li>" }
+
+        thanks_html = File.open("thanks.html", "r").readlines.join("")
+        send = thanks_html.gsub("<%= yield %>", output)
+        connection.puts send
+      end
   end
+  
   connection.close
 }
